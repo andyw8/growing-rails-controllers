7aa6872d9c4e5409414b2ee37f59d044139cc514
Andy Waite <github.aw@andywaite.com>
2015-04-12 21:35:30 +0100

    Remove unnecessary spec file

--- spec/models/note_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Note, :type => :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:text) }
-  it { should validate_presence_of(:published) }
-end
